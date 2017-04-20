22d25831efbb884dc32ee51db56b675e0f2da2fb
unpaleness <generalissimusunpaleness@gmail.com>
2017-04-21 00:00:30 +0300

    Issue #2 solved.

--- src/02.cpp
+++ src/02.cpp
@@ -4,42 +4,29 @@
 
 using namespace std;
 
-size_t find_target(const string &text, const string &unique, string &target, size_t d) {
-    for (size_t i = 0; i < d; ++i) {
-        // cout << ' ';
-    }
-    // cout << text << endl;
-    if (text.size() == 0 || text.size() < unique.size()) {
-        return 0;
-    }
-    if (text.size() == unique.size()) {
-        target = text;
-        return text.size();
-    }
-    bool is_target = true;
-    for (size_t i = 0; i < unique.size(); ++i) {
-        if (text.find(unique[i]) == string::npos) {
-            is_target = false;
+string find_target(const string &text, const string &unique) {
+    string target = text;
+    for (size_t len = unique.size(); len <= text.size(); ++len) {
+        bool is_target { false };
+        for (size_t i = 0; i < text.size() + 1 - len; ++i) {
+            target = text.substr(i, len);
+            bool is_unique { true };
+            for (size_t j = 0; j < unique.size(); ++j) {
+                if (target.find(unique[j]) == string::npos) {
+                    is_unique = false;
+                    break;
+                }
+            }
+            if (is_unique) {
+                is_target = true;
+                break;
+            }
+        }
+        if (is_target) {
             break;
         }
     }
-    if (!is_target) {
-        return 0;
-    }
-    string daughter1;
-    size_t length_daughter1 = find_target(text.substr(0, text.size() - 1), unique, daughter1, d + 1);
-    string daughter2;
-    size_t length_daughter2 = find_target(text.substr(1, text.size() - 1), unique, daughter2, d + 1);
-    if (length_daughter1 > 0 && length_daughter1 <= length_daughter2) {
-        target = daughter1;
-        return target.size();
-    }
-    if (length_daughter2 > 0) {
-        target = daughter2;
-        return target.size();
-    }
-    target = text;
-    return text.size();
+    return target;
 }
 
 int main() {
@@ -52,8 +39,7 @@ int main() {
         }
     }
     sort(unique.begin(), unique.end());
-    //cout << unique << endl;
     string target;
-    find_target(text, unique, target, 0);
+    target = find_target(text, unique);
     cout << target << endl;
 }
